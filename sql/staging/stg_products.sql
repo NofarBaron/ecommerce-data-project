11966c8cdf179d7b447b9b3c2788de9e63d94005
Nofar Baron <nofarbaron@unknow.local>
2026-05-19 21:07:17 +0300

    clean products catalog

---
+++ sql/staging/stg_products.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE TABLE `my-project-284-493514.ecommerce_data.stg_products` AS
+
+WITH cleaned AS (
+    SELECT
+        * except(category),
+        LOWER(
+            TRIM(
+                REGEXP_REPLACE(category, r'[^a-zA-Z0-9]', '')
+            )
+        ) AS cleaned_category
+        
+
+FROM `my-project-284-493514.ecommerce_data.product_catalog` 
+WHERE product_id IS NOT NULL
+)
+
+SELECT
+    * except(cleaned_category),
+    CASE
+        WHEN cleaned_category IN ('electronics','3l3ctronics','ele')
+            THEN 'Electronics'
+
+        WHEN cleaned_category IN ('beauty','b3auty','bea')
+            THEN 'Beauty'
+
+        WHEN cleaned_category IN ('sports')
+            THEN 'Sports'
+
+        WHEN cleaned_category IN ('automotive','automotiv3','aut')
+            THEN 'Automotive'
+
+        WHEN cleaned_category IN ('clothing','clo')
+            THEN 'Clothing'
+
+        WHEN cleaned_category IN ('home','hom','hom3')
+            THEN 'Home'
+
+        WHEN cleaned_category IN ('kitchen','kitch3n','kit')
+            THEN 'Kitchen'
+
+        WHEN cleaned_category IN ('toys','toy')
+            THEN 'Toys'
+
+        ELSE 'Unknown'
+    END AS category
+
+FROM cleaned;
